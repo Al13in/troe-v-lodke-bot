99021ce037a0fa6e1f59203202607d9920825ac3
Al13in <alinakrenc558@gmail.com>
2026-07-08 00:47:56 +0500

    Add 12 new TypeId handlers in Solver switch

--- ConsoleApp/Solver.cs
+++ ConsoleApp/Solver.cs
@@ -14,13 +14,25 @@ public class Solver
 
         return typeId switch
         {
-            "starter"        => SolverCaptain.SolveStarter(question),
-            "determinant"    => SolverCaptain.SolveMatrix(question),
-            "math"           => SolverOlesia.SolveSimpleMath(question),
-            "polynomial-root"=> SolverOlesia.SolvePolynomial(question),
-            "cypher"         => SolverJailed.SolveCypher(question),
-            "steganography"  => SolverJailed.SolveSteganography(question),
-            _                => "no roots"
+            "starter"                  => SolverCaptain.SolveStarter(question),
+            "determinant"             => SolverCaptain.SolveMatrix(question),
+            "inverse-matrix"          => SolverCaptain.SolveInverseMatrix(question),
+            "statistics"              => SolverCaptain.SolveStatistics(question),
+            "statistics-composition"  => SolverCaptain.SolveStatistics(question),
+            "json"                    => SolverCaptain.SolveJson(question),
+            "newton-json"             => SolverCaptain.SolveJson(question),
+            "string-number"           => SolverCaptain.SolveStringNumber(question),
+            "shape"                   => SolverCaptain.SolveShape(question),
+            "moment"                  => SolverCaptain.SolveMoment(question),
+            "math"                    => SolverOlesia.SolveSimpleMath(question),
+            "random-math"             => SolverOlesia.SolveSimpleMath(question),
+            "polynomial-root"         => SolverOlesia.SolvePolynomial(question),
+            "cypher"                  => SolverJailed.SolveCypher(question),
+            "taare-zameen-par-cypher" => SolverJailed.SolveCypher(question),
+            "steganography"           => SolverJailed.SolveSteganography(question),
+            "japanese-steganography"  => SolverJailed.SolveSteganography(question),
+            "japanese"                => SolverJailed.SolveSteganography(question),
+            _                         => ""
         };
     }
 }
