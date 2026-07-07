4dfc0c0d82710af34c1f6a151766441ec7dca173
Al13in <alinakrenc558@gmail.com>
2026-07-07 16:28:07 +0500

    роутинг по TypeId в Solver

--- ConsoleApp/Solver.cs
+++ ConsoleApp/Solver.cs
@@ -9,37 +9,18 @@ public class Solver
     public static string Solve(TaskResponse taskResponse)
     {
         string question = taskResponse.Question;
+        string typeId = taskResponse.TypeId ?? "";
+        string hint = taskResponse.UserHint ?? "";
 
-        if (question.Contains("Главный вопрос жизни"))
+        return typeId switch
         {
-            return SolverCaptain.SolveStarter(question);
-        }
-
-        if (question.Contains("\\\\") || question.Contains("&"))
-        {
-            return SolverCaptain.SolveMatrix(question);
-        }
-
-        if (question.Contains("x^2") || question.Contains("x") || taskResponse.UserHint.Contains("root"))
-        {
-            return SolverOlesia.SolvePolynomial(question);
-        }
-
-        if (Regex.IsMatch(question, @"\d+\s*[\+\-\*\/]\s*\d+"))
-        {
-            return SolverOlesia.SolveSimpleMath(question);
-        }
-
-        if (question.Contains("#reversed#"))
-        {
-            return SolverJailed.SolveCypher(question);
-        }
-
-        if (Regex.IsMatch(question, @"^[IVXLCDM]+\s*\n"))
-        {
-            return SolverJailed.SolveSteganography(question);
-        }
-
-        return "no roots"; 
+            "starter"        => SolverCaptain.SolveStarter(question),
+            "determinant"    => SolverCaptain.SolveMatrix(question),
+            "math"           => SolverOlesia.SolveSimpleMath(question),
+            "polynomial-root"=> SolverOlesia.SolvePolynomial(question),
+            "cypher"         => SolverJailed.SolveCypher(question),
+            "steganography"  => SolverJailed.SolveSteganography(question),
+            _                => "no roots"
+        };
     }
 }
